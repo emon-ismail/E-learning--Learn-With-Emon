8304655cd3eb458ca5ca23bc83c4f1dcae4b5b8a
Tonmoy-Roy <emonismail69@gmail.com>
2022-10-10 20:02:42 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    "primary": "#6419E6",
+    extend: {},
+  },
+  plugins: [],
+}
